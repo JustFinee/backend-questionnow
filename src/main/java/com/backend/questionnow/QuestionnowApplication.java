4b77bec5602eaf2044ea834b51a809b70433adf0
Bartłomiej Ziętek <bartlomiej.zietek@comarch.com>
2020-11-04 08:26:28 +0100

    Init project with Java 8

---
+++ src/main/java/com/backend/questionnow/QuestionnowApplication.java
@@ -0,0 +1,15 @@
+package com.backend.questionnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnowApplication.class, args);
+    }
+
+
+}
+
